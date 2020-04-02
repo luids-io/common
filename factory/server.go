620ef2e970dc4813c768e2201186297c4174086e
Luis Guillen Civera <luisguillenc@gmail.com>
2020-04-02 09:19:02 +0200

    add reuse grpc server in factory

--- factory/server.go
+++ factory/server.go
@@ -3,8 +3,10 @@
 package factory
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"sync"
 
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
@@ -16,8 +18,20 @@ import (
 	"github.com/luids-io/core/utils/ipfilter"
 )
 
+// ErrURIServerExists defines error when a server for the uri was created
+var ErrURIServerExists = errors.New("uri server already exists")
+
 // Server is a factory for a grpc server
 func Server(cfg *config.ServerCfg) (net.Listener, *grpc.Server, error) {
+	serverMutex.Lock()
+	defer serverMutex.Unlock()
+
+	// check in server pool
+	slis, srv, ok := serverPool.get(cfg.ListenURI)
+	if ok {
+		return slis, srv, ErrURIServerExists
+	}
+	// create server
 	err := cfg.Validate()
 	if err != nil {
 		return nil, nil, fmt.Errorf("invalid server config: %v", err)
@@ -29,13 +43,15 @@ func Server(cfg *config.ServerCfg) (net.Listener, *grpc.Server, error) {
 			return nil, nil, fmt.Errorf("initializing TLS: %v", err)
 		}
 	}
-	slis, err := grpctls.Listener(cfg.ListenURI)
+	slis, err = grpctls.Listener(cfg.ListenURI)
 	if err != nil {
 		return nil, nil, fmt.Errorf("listening server: %v", err)
 	}
 
-	srv := grpc.NewServer(getGRPCServerOpts(
+	srv = grpc.NewServer(getGRPCServerOpts(
 		creds, ipfilter.Whitelist(cfg.Allowed), cfg.Metrics)...)
+	//write in pool
+	serverPool.set(cfg.ListenURI, slis, srv)
 
 	return slis, srv, nil
 }
@@ -63,4 +79,32 @@ func getGRPCServerOpts(creds credentials.TransportCredentials, ipfilter ipfilter
 		grpcopts = append(grpcopts, grpc.Creds(creds))
 	}
 	return grpcopts
+}
+
+type grpcItem struct {
+	listener net.Listener
+	server   *grpc.Server
+}
+
+type grpcPool struct {
+	items map[string]grpcItem
+}
+
+func (p *grpcPool) get(uri string) (net.Listener, *grpc.Server, bool) {
+	item, ok := p.items[uri]
+	if !ok {
+		return nil, nil, false
+	}
+	return item.listener, item.server, true
+}
+
+func (p *grpcPool) set(uri string, lis net.Listener, srv *grpc.Server) {
+	p.items[uri] = grpcItem{listener: lis, server: srv}
+}
+
+var serverMutex sync.Mutex
+var serverPool grpcPool
+
+func init() {
+	serverPool = grpcPool{items: make(map[string]grpcItem)}
 }
